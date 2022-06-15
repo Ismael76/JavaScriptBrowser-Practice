7ea51fd1e8a51fadf443231e7608219a8775e416
Ismael Ali <ismaelali78@outlook.com>
2022-06-15 11:17:48 +0100

    Done all tasks

---
+++ index.js
@@ -0,0 +1,70 @@
+let mainHeader = document.querySelector("#header1");
+let subHeader = document.querySelector("#header2");
+
+let home = document.querySelector(".home");
+let about = document.querySelector(".about");
+let contact = document.querySelector(".contact");
+
+let homePara = document.querySelector(".home-para");
+let aboutPara = document.querySelector(".about-para");
+let contactPara = document.querySelector(".contact-para");
+
+home.addEventListener("click", () => {
+  if (
+    homePara.style.fontWeight === "bold" &&
+    homePara.style.fontSize === "20px"
+  ) {
+    homePara.style.fontWeight = "normal";
+    homePara.style.fontSize = "16px";
+  } else {
+    homePara.style.fontWeight = "bold";
+    homePara.style.fontSize = "20px";
+  }
+});
+
+about.addEventListener("click", () => {
+  if (
+    aboutPara.style.fontWeight === "bold" &&
+    aboutPara.style.fontSize === "20px"
+  ) {
+    aboutPara.style.fontWeight = "normal";
+    aboutPara.style.fontSize = "16px";
+  } else {
+    aboutPara.style.fontWeight = "bold";
+    aboutPara.style.fontSize = "20px";
+  }
+});
+
+contact.addEventListener("click", () => {
+  if (
+    contactPara.style.fontWeight === "bold" &&
+    contactPara.style.fontSize === "20px"
+  ) {
+    contactPara.style.fontWeight = "normal";
+    contactPara.style.fontSize = "16px";
+  } else {
+    contactPara.style.fontWeight = "bold";
+    contactPara.style.fontSize = "20px";
+  }
+});
+
+let body = document.querySelector("body");
+
+let paragraph = document.createElement("p");
+
+mainHeader.addEventListener("click", () => {
+  mainHeader.style.backgroundColor = "red";
+});
+
+mainHeader.addEventListener("mouseout", () => {
+  mainHeader.style.backgroundColor = "white";
+});
+
+subHeader.addEventListener("mouseover", () => {
+  subHeader.textContent = "Welcome to the website!";
+});
+
+window.addEventListener("keydown", (e) => {
+  paragraph.textContent = e.key;
+  body.append(paragraph);
+});
